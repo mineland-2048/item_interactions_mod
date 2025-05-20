bcd8581b658f189fe00327b8176bb990e016c2c8
mineland <mineland222@gmail.com>
2025-05-19 21:39:25 -0600

    Made some client side movement and removed more leftovers

--- fabric/src/main/java/dev/mineland/item_interactions_mod/fabric/client/Item_interactions_modFabricClient.java
+++ fabric/src/main/java/dev/mineland/item_interactions_mod/fabric/client/Item_interactions_modFabricClient.java
@@ -1,10 +1,23 @@
 package dev.mineland.item_interactions_mod.fabric.client;
 
+import dev.mineland.item_interactions_mod.Item_interactions_mod;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
+import net.fabricmc.fabric.api.resource.ResourcePackActivationType;
+import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
 
 public final class Item_interactions_modFabricClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         // This entrypoint is suitable for setting up client-specific logic, such as rendering.
+
+        Item_interactions_mod.init();
+
+        FabricLoader.getInstance().getModContainer(Item_interactions_mod.MOD_ID).ifPresent(container -> {
+            ResourceLocation packId = ResourceLocation.fromNamespaceAndPath(Item_interactions_mod.MOD_ID, "example_gui_particles");
+            ResourceManagerHelper.registerBuiltinResourcePack(packId, container, Component.literal("Example gui particle pack"), ResourcePackActivationType.NORMAL);
+        });
     }
 }
