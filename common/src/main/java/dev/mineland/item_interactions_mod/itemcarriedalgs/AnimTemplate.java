9880bb0845dab2f5a56b45822e53a62887b2690a
mineland <mineland222@gmail.com>
2025-05-31 01:17:32 -0600

    Clean up

--- common/src/main/java/dev/mineland/item_interactions_mod/itemcarriedalgs/AnimTemplate.java
+++ common/src/main/java/dev/mineland/item_interactions_mod/itemcarriedalgs/AnimTemplate.java
@@ -9,27 +9,4 @@ import static dev.mineland.item_interactions_mod.GlobalDirt.*;
 
 public class AnimTemplate {
 
-
-    public static void setVariables() {
-        GlobalDirt.currentMilis = Util.getMillis();
-
-        tickRate = 20;
-
-        GlobalDirt.tickScale = tickRate / 20;
-
-        GlobalDirt.frameTime = currentMilis - lastMilis;
-        GlobalDirt.frameDelta = ((frameTime) / 1000f);
-
-
-
-    }
-
-    public static void setLastVariables() {
-        msCounter += frameDelta;
-        msCounter %= 1000;
-
-        lastMilis = currentMilis;
-
-
-    }
 }
